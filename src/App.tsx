943108e5832d812fb2a8d224fdc7a329e7e29ad4
MV <mv@Mac.hbs.edu>
2026-03-30 21:38:51 -0400

    feat: add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { CartProvider } from "@/context/CartContext";
 import Index from "./pages/Index";
 import Cart from "./pages/Cart";
@@ -16,6 +17,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <CartProvider>
+        <Analytics />
         <Toaster />
         <Sonner />
         <BrowserRouter>
